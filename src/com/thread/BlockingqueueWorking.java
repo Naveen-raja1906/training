3263fdb2c9e988b2a37c9aa317974ea029531876
Naveen Raja <dnaveenraja@gmail.com>
2020-04-21 23:27:10 +0530

    added Thread names

--- src/com/thread/BlockingqueueWorking.java
+++ src/com/thread/BlockingqueueWorking.java
@@ -24,7 +24,7 @@ class BlockingQueue {
         	System.out.println("Empty Queue");
             notifyAll(); 
         } 
-        System.out.println("enqueue : "+item);
+        System.out.println("enqueue : "+item+" : "+ Thread.currentThread().getName());
         this.queue.add(item); 
     } 
   
@@ -40,16 +40,16 @@ class BlockingQueue {
             notifyAll(); 
         } 
   
-        System.out.println("dequeue : "+this.queue.remove(0));
+        System.out.println("dequeue : "+this.queue.remove(0)+" : "+ Thread.currentThread().getName());
     } 
 } 
 
 class Producerbq implements Runnable{
 	BlockingQueue bq;
 	
-	public Producerbq(BlockingQueue bq) {
+	public Producerbq(BlockingQueue bq,String name) {
 		this.bq = bq;
-		Thread t = new Thread(this,"Producer");
+		Thread t = new Thread(this,name);
 		t.start();
 	}
 	
@@ -69,9 +69,9 @@ class Producerbq implements Runnable{
 class Consumerbq implements Runnable{
 	BlockingQueue bq;
 	
-	public Consumerbq(BlockingQueue bq) {
+	public Consumerbq(BlockingQueue bq,String name) {
 		this.bq = bq;
-		Thread t = new Thread(this,"Consumer");
+		Thread t = new Thread(this,name);
 		t.start();
 	}
 	
@@ -91,9 +91,9 @@ public class BlockingqueueWorking{
 	
 	public static void main(String[] args) {
 		BlockingQueue bq = new BlockingQueue(10);
-		new Producerbq(bq);
-		new Producerbq(bq);
-		new Consumerbq(bq);
-		new Consumerbq(bq);
+		new Producerbq(bq,"Producer1");
+		new Producerbq(bq,"Producer2");
+		new Consumerbq(bq,"Consumer1");
+		new Consumerbq(bq,"Consumer2");
 	}
 }
